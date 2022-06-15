da2aba5d62910e0292f855daa4172ef84315af7d
Witty Wayne <wallycodes.noreply@gmail.com>
2022-06-16 01:31:27 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; //es6 modules --> piece of functionality into our code 
+import ReactDom from "react-dom"; // contains the render method
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Greeting(){// captitalize the name --> react knows that this is a component
+  return <h4>this is debj and this is first component</h4>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDom.render(<Greeting/>, document.getElementById('root')) // looking for what we want to render and where
+
+// we need to self close or two tags like <Greeting></Greeting>
